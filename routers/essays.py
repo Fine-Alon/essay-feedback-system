3c0ac925282c5a577bf124d4be02a956a61becb6
Alon Fine <mecheniy5462@gmail.com>
2026-06-10 11:16:58 +0300

    Routs: added routs for .txt & user input files.  CRUD: 3 funtions are done

--- routers/essays.py
+++ routers/essays.py
@@ -1,4 +1,110 @@
 # this part will accept .txt
 # Simple!!! GET data return RESPOnce
 
-# @app.post("/upload-essay")
+from fastapi import APIRouter, UploadFile, File, Form, HTTPException
+from services.storage import create_essay_record
+from constants import _ESSAY_MIN_LENGTH
+
+# In main.py it will be connected by prefix   /essays...
+router = APIRouter()
+
+
+@router.post("/upload/file")
+async def upload_essay_file(
+    user_id: str = Form(...),  # get user ID out of form
+    file: UploadFile = File(...),  # get file out of form
+):
+    """
+    Endpoint to upload essay by  file.txt
+    Get user ID and file.txt, read it,
+    decode to str and send to be saved as JSON.
+    """
+
+    # Validation: check that file is  .txt format
+    if not file.filename.endswith(".txt"):
+        raise HTTPException(
+            status_code=400, detail="Format is not correct. Allowed  .txt  format only"
+        )
+
+    try:
+        # Async reading, get bytes
+        file_bytes = await file.read()
+
+        # Decoding: transform bytes into regular Python str
+        # 'utf-8' very important for reading another languages
+        essay_text = file_bytes.decode("utf-8")
+
+        # Check for empty file
+        if not essay_text.strip():
+            raise HTTPException(
+                status_code=400,
+                detail="File is empty. Please, upload file with content/essay",
+            )
+
+        # Saving: function call, to create JSON file
+        essay_id = create_essay_record(user_id=user_id, text=essay_text)
+
+        # Client answer: return status SUCCESS and essay ID
+        return {
+            "success": True,
+            "message": f"File '{file.filename}' successfully uploaded and saved",
+            "essay_id": essay_id,
+            "status": "pending",
+        }
+
+    except UnicodeDecodeError:
+        # If file is saved in wrong encoding (not UTF-8), raise exception
+        raise HTTPException(
+            status_code=400,
+            detail="Encoding of file is wrong. Make shore, that file is saved as UTF-8.",
+        )
+    except Exception as e:
+        # Another exception
+        raise HTTPException(
+            status_code=500, detail=f"Server error is occurred: {str(e)}"
+        )
+
+
+@router.post("/upload/text")
+async def upload_essay_text(
+    user_id: str = Form(...),  # get user ID
+    text: str = Form(...),  # get text out of form
+):
+    """
+    Endpoint for uploading essay by text field on client side by typing
+    Accept user ID and text, then save it
+    """
+
+    # Validation: empty form send check
+    clean_text = text.strip()
+
+    if not clean_text:
+        raise HTTPException(
+            status_code=400,
+            detail="Text content not found. Please, enter text",
+        )
+
+    # Minimum length check
+    if len(clean_text) < _ESSAY_MIN_LENGTH:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Text too short. Essay must be at least {_ESSAY_MIN_LENGTH} symbols",
+        )
+
+    try:
+        # Saving
+        essay_id = create_essay_record(user_id=user_id, text=clean_text)
+
+        # Response to client
+        return {
+            "success": True,
+            "message": "Essay is successfully uploaded",
+            "essay_id": essay_id,
+            "status": "pending",
+        }
+
+    except Exception as e:
+        # Catch another server errors
+        raise HTTPException(
+            status_code=500, detail=f"Internal server error occurred: {str(e)}"
+        )
--- services/storage.py
+++ services/storage.py
@@ -1,7 +1,11 @@
-# Импортируем нашу функцию из папки utils
+import json
+import os
 from utils.id_generator import generate_uuid
 
+# C R U D --->
 
+
+# CREATE
 def create_essay_record(user_id: str, text: str):
     # generate ID for new essay
     new_essay_id = generate_uuid()
@@ -9,19 +13,104 @@ def create_essay_record(user_id: str, text: str):
     # Create file name (Like: '550e8400-e29b-41d4.json')
     filename = f"{new_essay_id}.json"
 
+    # path to save in (папка data/essays/)
+    filepath = os.path.join("data", "essays", filename)
+
     # Forming data structure: JSON
     essay_data = {
         "essay_id": new_essay_id,
         "user_id": user_id,
         "original_text": text,
-        "status": "pending",
+        "status": "pending",  # PENDING!!!
         "analysis_results": {},
     }
 
-    # Then will be script to save dict: essay_data to file...
+    # Then will be script to save dict: essay_data to file(DB)...
     print(f"Ready to save in file: {filename}")
-    return new_essay_id
+
+    with open(filepath, "w", encoding="utf-8") as f:
+        # dump make Python dict as JSON format and record to file
+        json.dump(essay_data, f, ensure_ascii=False, indent=4)
+
+    return new_essay_id  # return ID helps reach this essay later on Client...
 
 
-def save_essay_to_json(data):
-    pass
+# READ
+def get_essay(essay_id: str) -> dict:
+    """
+    Look for essay JSON file by its ID, reade it and return as dict.
+    In case file NOT found, return None.
+    """
+
+    filename = f"{essay_id}.json"
+    filepath = os.path.join("data", "essays", filename)  # get right path to file
+
+    #  Check if file exist
+    if not os.path.exists(filepath):
+        print(f"Error: Essay with {essay_id} ID not found")
+        return None
+
+    try:
+        with open(filepath, "r", encoding="utf-8") as f:
+            # json.load takes text from JSON file and turn it to Python ( dict !!!)
+            essay_data = json.load(f)
+
+        return essay_data
+
+    except json.JSONDecodeError:
+        # In case file was corrupted and no longer valid JSON
+        print(f"Error: File {filepath} corrupted!")
+        return None
+
+
+## OREN - usage
+# from services.storage import get_essay
+
+# def analyze_essay_text(essay_id: str):
+## OREN ask for data
+# data = get_essay(essay_id)
+
+# if data is None:
+#   return "No essay found!"
+
+## OREN - get any text by key "original_text"
+# text_to_check = data["original_text"]
+
+## OREN - run any checks
+
+
+# UPDATE
+def update_essay_results(essay_id: str, analysis_data: dict) -> bool:
+    """
+    Update essay JSON file with results of checking
+    Change status 'completed' and record data from Oren's checking functions
+    return True in case of success and False if there is Error.
+    """
+    filename = f"{essay_id}.json"
+    filepath = os.path.join("data", "essays", filename)
+
+    # Check if file exists
+    if not os.path.exists(filepath):
+        print(f"Update error: Essay with {essay_id} ID not found")
+        return False
+
+    try:
+        # Reading
+        with open(filepath, "r", encoding="utf-8") as f:
+            essay_data = json.load(f)
+
+        # Updating of the appropriate fields in the dict
+        essay_data["analysis_results"] = analysis_data
+        essay_data["status"] = "completed"
+
+        # Recording new updated content
+        with open(filepath, "w", encoding="utf-8") as f:
+            json.dump(essay_data, f, ensure_ascii=False, indent=4)
+
+        print(f"Success: Results for the essay {essay_id} were saved")
+        return True
+
+    except Exception as e:
+        # Here we catch any exceptions.
+        print(f"Critical issue while saving results for {essay_id}: {str(e)}")
+        return False
